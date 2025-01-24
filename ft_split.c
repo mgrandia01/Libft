10a300a587b0ba85cb5abecdeddef5d4117875fa
mgrandia01 <marta.grandia01@gmail.com>
2025-01-24 18:21:13 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,12 @@
 /*   By: mgrandia <mgrandia@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 16:24:37 by mgrandia          #+#    #+#             */
-/*   Updated: 2025/01/21 20:11:15 by mgrandia         ###   ########.fr       */
+/*   Updated: 2025/01/12 17:47:36 by mgrandia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"
+
 
 int	ft_countwords(char const *s, char c)
 {
@@ -22,6 +22,7 @@ int	ft_countwords(char const *s, char c)
 	count = 0;
 	in_word = 0;
 	i = 0;
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
@@ -43,6 +44,7 @@ void	ft_split_free(char **ptr)
 	if (!ptr)
 		return ;
 	i = 0;
+
 	while (ptr[i])
 	{
 		free (ptr[i]);
@@ -53,28 +55,67 @@ void	ft_split_free(char **ptr)
 
 char	**ft_split(char const *s, char c)
 {
-	char	**lst;
-	size_t	word_len;
-	int		i;
+	char	**ptr;
+	int	i;
+	int	first;
+	int 	word;
+	int	k;
 
-	lst = (char **)malloc((ft_countwords(s, c) + 1) * sizeof(char *));
-	if (!s || !lst)
-		return (0);
-	i = 0;
-	while (*s)
+	ptr = (char **)malloc((ft_countwords(s,c)+1)*sizeof(char *));
+	if (!ptr)
 	{
-		while (*s == c && *s)
-			s++;
-		if (*s)
-		{
-			if (!ft_strchr(s, c))
-				word_len = ft_strlen(s);
-			else
-				word_len = ft_strchr(s, c) - s;
-			lst[i++] = ft_substr(s, 0, word_len);
-			s += word_len;
-		}
+		free (ptr);
+		return (NULL);
 	}
-	lst[i] = NULL;
-	return (lst);
+	i = 0;
+	first = 0;
+	word = 0;
+	k = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c || s[i+1] == '\0')
+		{
+			if (i > first)
+			{
+				if (s[i] != c && s[i+1] == '\0')
+					i++;
+				ptr[word] = (char *)malloc(( i - first +1)*sizeof(char));//sizeof -last
+				if (!ptr[word])
+				{
+					ft_split_free(ptr);
+					return (NULL);
+				}
+				k = first;
+				while (k < i)
+				{
+					ptr[word][k - first] = s[k];
+					k ++;
+				}
+				ptr[word][k-first] = '\0';
+				word ++;
+				if (s[i-1] != c && s[i] == '\0')//perque no sumi dos vegades i al final i dongui error
+					i--;
+
+			}
+			else if (s[i] != c && s[i+1] == '\0' && s[i-1] == c)
+			{
+			  i++;	
+				ptr[word] = (char *)malloc(( i - first +1)*sizeof(char));
+				k = first;
+				while (k < i)
+				{
+					ptr[word][k - first] = s[k];
+					k ++;
+				}
+				ptr[word][i-first] = '\0';
+				word ++;
+				i--;
+			}
+			first = i+1;	
+		}
+		i ++;
+	}
+	ptr[word] = NULL;
+	return (ptr);
 }
